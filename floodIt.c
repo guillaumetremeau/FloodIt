c042bc90bc0fa07ddb5158c46580f337306a203f
Guillaume Tremeau <tremeauguillaume@gmail.com>
2016-10-13 11:55:15 +0200

    init main

---
+++ floodIt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COULEUR 6
+#define TAILLE 12
+
+void initialiser(int[] tab);
+void afficherGrille(int[] tab);
+void afficherIteration(int[] tab);
+
+
+int main(){
+
+	return 0;
+}
+
+
+void initialiser(int[] tab){
+
+}
+void afficherGrille(int[] tab){
+
+}
+void afficherIteration(int[] tab){
+	
+}
